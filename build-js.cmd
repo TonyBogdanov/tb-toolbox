290c1e61955510656acd9dea515ce3112dcda0a2
Tony Bogdanov <tonybogdanov@gmail.com>
2016-01-23 14:38:46 +0200

    a bit better structure

--- build-js.cmd
+++ build-js.cmd
@@ -1,6 +1,25 @@
 @echo off
 @cls
 
-del /F /Q .\js\toolbox.js >nul 2>&1
-copy /B .\js\frame\frame.js+.\js\transition-end\transition-end.js+.\js\count-to\count-to.js+.\js\resize-it\resize-it.js+.\js\scroll-direction\scroll-direction.js+.\js\scroll-to-this\scroll-to-this.js+.\js\linking\linking.js .\js\toolbox.js >nul 2>&1
-minify .\js\toolbox.js
+del /Q /F dist
+
+copy /B js\frame\frame.js dist\frame.js >nul 2>&1
+copy /B js\transition-end\transition-end.js dist\transition-end.js >nul 2>&1
+copy /B js\count-to\count-to.js dist\count-to.js >nul 2>&1
+copy /B js\resize-it\resize-it.js dist\resize-it.js >nul 2>&1
+copy /B js\scroll-direction\scroll-direction.js dist\scroll-direction.js >nul 2>&1
+copy /B js\scroll-to-this\scroll-to-this.js dist\scroll-to-this.js >nul 2>&1
+copy /B js\linking\linking.js dist\linking.js >nul 2>&1
+
+cmd /c minify dist\frame.js >nul 2>&1
+cmd /c minify dist\transition-end.js >nul 2>&1
+cmd /c minify dist\count-to.js >nul 2>&1
+cmd /c minify dist\resize-it.js >nul 2>&1
+cmd /c minify dist\scroll-direction.js >nul 2>&1
+cmd /c minify dist\scroll-to-this.js >nul 2>&1
+cmd /c minify dist\linking.js >nul 2>&1
+
+copy /B dist\frame.js+dist\transition-end.js+dist\count-to.js+dist\resize-it.js+dist\scroll-direction.js+dist\scroll-to-this.js+dist\linking.js dist\toolbox.js >nul 2>&1
+cmd /c minify dist\toolbox.js >nul 2>&1
+
+exit /B
